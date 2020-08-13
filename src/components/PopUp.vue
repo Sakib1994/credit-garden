c0930c63986545030c0a7a65ce3621c6717a00b0
Md. Nazmus Sakib <mdnazmussakib@pop-os.localdomain>
2020-08-13 10:14:24 +0600

    complete email Send

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -19,19 +19,32 @@
             <v-card class="white--text text-center" color="#223560" width="400">
               <v-card-text>
                 <p class="text-h5 white--text my-1">Risk Profile</p>
-                <p class="text-h3 white--text mt-1 mb-0">{{score[3]}}</p>
+                <p class="text-h3 white--text mt-1 mb-0">{{ score[3] }}</p>
                 <p class="text-subtitle-1 white--text my-0">Credit Score</p>
-                <p class="text-h4 white--text mt-2">{{score[1]}}</p>
+                <p class="text-h4 white--text mt-2">{{ score[1] }}</p>
                 <p class="text-subtitle-1 white--text my-2">Risk Level</p>
               </v-card-text>
 
               <v-card-actions class="ml-3">
                 <v-row class="my-0">
                   <v-col cols="9" class="my-0">
-                    <v-text-field label="Get Result On Your Email" outlined type="email" dense dark></v-text-field>
+                    <v-text-field
+                      label="Get Result On Your Email"
+                      outlined
+                      type="email"
+                      dense
+                      dark
+                      v-model="email"
+                    ></v-text-field>
                   </v-col>
                   <v-col cols="3" class="my-0">
-                    <v-btn class="mt-1 pr-2" color="green" dark>Send</v-btn>
+                    <v-btn
+                      class="mt-1 pr-2"
+                      color="green"
+                      @click="sendMail"
+                      dark
+                      >Send</v-btn
+                    >
                   </v-col>
                 </v-row>
               </v-card-actions>
@@ -43,6 +56,7 @@
       <v-divider></v-divider>
 
       <v-card-actions>
+        <v-btn color="secondary" outlined @click="logData">log Data</v-btn>
         <v-spacer></v-spacer>
         <v-btn color="primary" text @click="flag.dialog = false">close</v-btn>
       </v-card-actions>
@@ -51,16 +65,37 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  props: {
-    flag: Object,
-    score: Array,
-  },
-  mounted() {
-    console.log(this.score);
+  props: ["flag", "score", "user_id"],
+  data: () => ({
+    email: "",
+  }),
+  methods: {
+    logData() {
+      console.log(this.user_id);
+    },
+    sendMail() {
+      const data = {
+        email: this.email,
+        user_id: this.user_id,
+        score: this.score,
+      };
+      const url ="http://creditscoreapi-env-1.eba-7zmfpp7h.us-east-2.elasticbeanstalk.com/sendmail";
+
+      try {
+        axios.post(url, data).then(({status, data}) => {
+          if (status === 200) {
+            console.log(data.message);
+          }
+        });
+        console.log("Email sent");
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -4,10 +4,19 @@
     <v-row justify="center">
       <v-col cols="8">
         <app-demographics @next="tracker = 2" v-if="tracker === 1" />
-        <app-work-experience @prev="tracker = 1" @next="tracker = 3" v-if="tracker === 2" />
-        <app-finantial-info @prev="tracker = 2" @submit="submmit" v-if="tracker === 3" />
+        <app-work-experience
+          @prev="tracker = 1"
+          @next="tracker = 3"
+          v-if="tracker === 2"
+        />
+        <app-finantial-info
+          @prev="tracker = 2"
+          @submit="submmit"
+          v-if="tracker === 3"
+        />
         <!-- pop up -->
-        <app-pop-up :flag="flag" :score="score"></app-pop-up>
+        <v-btn @click="submmit"></v-btn>
+        <app-pop-up :flag="flag" :score="score" :user_id="user_id"></app-pop-up>
       </v-col>
     </v-row>
   </div>
@@ -31,22 +40,75 @@ export default {
     tracker: 1,
     flag: { dialog: false },
     score: [],
+    user_id: "",
   }),
   methods: {
     submmit() {
       const url =
         "http://creditscoreapi-env-1.eba-7zmfpp7h.us-east-2.elasticbeanstalk.com/userresponse";
-      const data = this.$store.getters.getAllStates;
-
-      console.log(data);
-      axios.post(url, data).then(({ data, status }) => {
-        if (status === 200) {
-          console.log(data.message.score);
-          this.score = data.message.score;
-          this.flag.dialog = true;
-        }
-      });
-      console.log("Submitted");
+      //const data = this.$store.getters.getAllStates;
+      const info = {
+        address1yrs: 20,
+        address1stat: "Rent",
+        address1type: "Apartment",
+        address2yrs: 10,
+        address2stat: "Rent",
+        address2type: "Apartment",
+        marital_status: "Married",
+        gender: "Male",
+        num_depend: 2,
+        num_dep_living_with_you: 2,
+        age: 48,
+        employ_types1: "Full-time",
+        employ_det1: "Manager",
+        yrs_posn1: 8,
+        industry1: "Accountancy & Finance",
+        employ_types2: "Full-time",
+        employ_det2: "Junior",
+        yrs_posn2: 10,
+        industry2: "Accountancy & Finance",
+        employ_types3: "Full-time",
+        employ_det3: "Entry-Level",
+        yrs_posn3: 3,
+        industry3: "Accountancy & Finance",
+        loan_amt: 100000,
+        loan_term_mths: 48,
+        secured_flag: "Secured",
+        asset_value: 10000,
+        asset_type: "Home",
+        loan_purpose: "Home Improvement",
+        credit_card: 1000,
+        mortgage: 1000,
+        car_loan: 1000,
+        other_loan: 1000,
+        rent: 2000,
+        utilities: 500,
+        ins_pymt: 500,
+        life_ins_prem: 500,
+        transport_exp: 500,
+        living_exp: 2000,
+        education_exp: 1000,
+        medical: 500,
+        entertainment: 200,
+        other_income: 2000,
+        net_mthly_income: 10000,
+        other_expenses: 500,
+        bankrupt_flag: "No",
+        bankrupt_time_yrs: 0,
+      };
+      try {
+        axios.post(url, info).then(({ data, status }) => {
+          if (status === 200) {
+            this.score = data.message.score;
+            this.user_id = data.message.user_id;
+            this.flag.dialog = true;
+            console.log(this.user_id, this.score);
+          }
+        });
+        console.log("Submitted");
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
